2134d0997eca2ce46545bf25b7796a491a999d25
xXGhananXx <82243135+xXGhananXx@users.noreply.github.com>
2022-01-01 11:17:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function player_update()
+{
+    fabric.Image.fromURL("player.png", function(Img) {
+        player_object = Img;
+
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);
+
+    });
+}
+
+function new_image(get_image)
+{
+    fabric.Image.fromURL(get_image, function(Img) {
+        block_image_object = Img;
+
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+        
+        top:player_y,
+        left:player_x
+        });
+        canvas.add(block_image_object);
+
+    });
+}
